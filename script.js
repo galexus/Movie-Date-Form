c262c4d43f1dbf60f462bc32600fa6a78b3c026c
galexus <oluwanifemijesuseun@gmail.com>
2019-07-12 19:48:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// js to accept or decline date form request
+let popup = prompt ("Do you accept date request?")
+
+if (popup = "no") {
+    alert("EXIT PAGE");
+}
+else {
+    alert("Fill up the form");
+}
